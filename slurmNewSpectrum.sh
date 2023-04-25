a0db070364bdd1fb2c43d6b0d174253c932f3238
Fernando Spadea <ffs006@bucknell.edu>
2023-04-24 20:31:38 -0400

    gpu 1

--- slurmNewSpectrum.sh
+++ slurmNewSpectrum.sh
@@ -6,4 +6,4 @@ sMult=$3
 dataSizeValue=$4
 DIR="./"
 
-sbatch -p el8 -N $COMPUTE_NODES --gres=gpu:4 --mail-type=ALL --mail-user=fespadearocks@gmail.com -t 5 -D $DIR -o $DIR/proj$1-$2-$3-$4.stdout -e $DIR/proj$1-$2-$3-$4.stderr $DIR/run_job.sh $RANKS_PER_NODE $sMult $dataSizeValue
+sbatch -p el8 -N $COMPUTE_NODES --gres=gpu:1 --mail-type=ALL --mail-user=fespadearocks@gmail.com -t 5 -D $DIR -o $DIR/proj$1-$2-$3-$4.stdout -e $DIR/proj$1-$2-$3-$4.stderr $DIR/run_job.sh $RANKS_PER_NODE $sMult $dataSizeValue
